e1cb7b9b62e6ccc9c05fea7e01d13a325f209859
Scesss <kiriell55@gmail.com>
2025-01-06 13:39:10 +0300

    new token

--- script.py
+++ script.py
@@ -1,8 +1,11 @@
 import telebot
+import os
+from dotenv import load_dotenv
 from telebot import types
 
-token='7825442251:AAFKoWTmKNT0RwrnZpivKTpyImiURchckGY'
-bot = telebot.TeleBot(token)
+
+bot = telebot.TeleBot(os.getenv('BOT_TOKEN'))
+admin_chat_id = os.getenv('ADMIN_CHAT_ID')
 
 # @bot.message_handler(commands=['start'])
 # def send_welcome(message):
@@ -12,6 +15,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
 
     # markup = types. ReplyKeyboardMarkup()
     # btn1 = types. KeyboardButton ('Лекейти на сайт', url= 'https://g00/glp.com')
